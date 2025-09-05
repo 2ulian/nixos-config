0f0a804d1c1533bd634de5bbffe7b42d0f2d289c
Julian <rayconstantyjulian@gmail.com>
2025-09-06 00:04:03 +0200

    Minor change

--- profiles/laptop-nixos/home.nix
+++ profiles/laptop-nixos/home.nix
@@ -6,7 +6,7 @@
     ../../home-modules/base.nix
     #../../home-modules/dwm.nix
     ../../home-modules/hyprland/hyprland.nix
-    ../../home-modules/hyprland/illogical-impulse.nix
+    ../../home-modules/hyprland/caelestia.nix
   ];
 
   home.packages = [
