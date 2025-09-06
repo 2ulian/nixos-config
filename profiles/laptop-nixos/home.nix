800a839eb0252254eb60c3405b9fdea7073f53ef
Julian <rayconstantyjulian@gmail.com>
2025-09-07 00:29:48 +0200

    Minor changes

--- profiles/laptop-nixos/home.nix
+++ profiles/laptop-nixos/home.nix
@@ -4,9 +4,9 @@
   imports = [
     ./shellScript.nix
     ../../home-modules/base.nix
-    #../../home-modules/dwm.nix
     ../../home-modules/hyprland/hyprland.nix
     ../../home-modules/hyprland/caelestia.nix
+    #../../home-modules/hyprland/illogical-impulse.nix
   ];
 
   xdg.configFile."hypr/hyprland.conf".source = lib.mkOverride 10 ../../dotfiles/hypr/laptop.conf;
