deb4f4a68d88b4ba1205f9832876b6e74ded42b8
Julian <rayconstantyjulian@gmail.com>
2025-11-04 00:41:25 +0100

    Removed ublock origin forced install in order to be able to manually update it

--- home-modules/librewolf.nix
+++ home-modules/librewolf.nix
@@ -14,7 +14,6 @@
         # uBlock Origin:
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
         };
 
         # Pywalfox
