e7cd4aa9a1fba62222e68d3b17298546b7b20eb7
Julian <rayconstantyjulian@gmail.com>
2025-12-11 12:13:36 +0100

    syntax correction

--- modules/dwm.nix
+++ modules/dwm.nix
@@ -6,10 +6,11 @@
   services.xserver = {
     enable = true;
     xkb.layout = "fr";
-    xkb.options= "lv3:rwin_switch";
+    xkb.options = "lv3:rwin_switch";
     displayManager.startx.enable = true;
-    libinput.naturalScrolling = true;
   };
+
+  services.libinput.touchpad.naturalScrolling = true;
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
