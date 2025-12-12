ce6327214e0e0d6e6f4df893360e8934899b53a8
Julian <rayconstantyjulian@gmail.com>
2025-12-12 12:01:59 +0100

    Removed hyprland from laptop configuration

--- profiles/macbook/home.nix
+++ profiles/macbook/home.nix
@@ -7,7 +7,7 @@
   imports = [
     ../../home-modules/base.nix
     ../../home-modules/dwm.nix
-    ../../home-modules/hyprland/hyprland.nix
+    # ../../home-modules/hyprland/hyprland.nix
     #../../home-modules/hyprland/caelestia.nix
     ../../home-modules/hyprland/illogical-impulse.nix
   ];
