902c500cf33ba552032d2b78862f234bb4c1cd54
Julian <rayconstantyjulian@gmail.com>
2026-01-23 08:52:38 +0100

    laptop-nixos configuration update

--- profiles/laptop-nixos/configuration.nix
+++ profiles/laptop-nixos/configuration.nix
@@ -8,11 +8,17 @@
     ../../modules/intel-drivers.nix
     ../../modules/battery.nix
     ../../modules/gaming.nix
+    ../../modules/grub.nix
     /etc/nixos/hardware-configuration.nix
   ];
 
   networking.hostName = "T480"; # Define your hostname.
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Battery optimisations
   services.thermald.enable = true;
+
+  services.openssh = {
+    enable = true;
+  };
 }
