9ad4be4b84d62138cecdbdff400e88f0d7e07161
Julian <rayconstantyjulian@gmail.com>
2026-02-28 18:52:22 +0100

    virtualization chagnes

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -3,23 +3,41 @@
   pkgs,
   ...
 }: {
-  # Manage the virtualization services
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        swtpm.enable = true;
+  specialisation = {
+    kvm.configuration = {
+      virtualisation = {
+        libvirtd = {
+          enable = true;
+          qemu = {
+            swtpm.enable = true;
+          };
+        };
+        spiceUSBRedirection.enable = true;
       };
+
+      boot.kernelParams = [
+        "intel_iommu=on" # or "amd_iommu=on"
+        "iommu.strict=1"
+      ];
+
+      services.spice-vdagentd.enable = true;
+
+      users.users.fellwin.extraGroups = ["libvirtd"];
     };
-    spiceUSBRedirection.enable = true;
+
+    virtualbox.configuration = {
+      # Désactive KVM AU NIVEAU KERNEL
+      boot.blacklistedKernelModules = [
+        "kvm"
+        "kvm_intel"
+        "kvm_amd"
+      ];
+
+      # Active VirtualBox
+      virtualisation.virtualbox.host.enable = true;
+
+      # Optionnel mais recommandé
+      virtualisation.virtualbox.host.enableExtensionPack = true;
+    };
   };
-
-  boot.kernelParams = [
-    "intel_iommu=on" # or "amd_iommu=on"
-    "iommu.strict=1"
-  ];
-
-  services.spice-vdagentd.enable = true;
-
-  users.users.fellwin.extraGroups = ["libvirtd"];
 }
