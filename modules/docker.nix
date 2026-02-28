5c48fa56a162455adae7b0fefbb5b3fd0db20a66
Julian <rayconstantyjulian@gmail.com>
2026-02-28 17:49:20 +0100

    modified docker files

--- modules/docker.nix
+++ modules/docker.nix
@@ -4,8 +4,13 @@
   lib,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    lazydocker
+  ];
+
   users.users.fellwin.extraGroups = [
     "docker"
   ];
+
   virtualisation.docker.enable = true;
 }
